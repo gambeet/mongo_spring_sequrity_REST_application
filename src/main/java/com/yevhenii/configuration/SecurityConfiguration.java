97273532ade0a1b9ba1cc1b01fe5ac9b27407c88
gambeet <yevhenii.pshe@gmail.com>
2017-12-04 15:48:15 +0200

    Added token authentication

--- src/main/java/com/yevhenii/configuration/SecurityConfiguration.java
+++ src/main/java/com/yevhenii/configuration/SecurityConfiguration.java
@@ -1,16 +1,18 @@
 package com.yevhenii.configuration;
 
+import com.yevhenii.configuration.filters.JWTAuthenticationFilter;
+import com.yevhenii.configuration.filters.JWTLoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 /**
  * Created by Yevhenii on 04.12.2017.
@@ -26,6 +28,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("password")
+//                .roles("ADMIN");
 //        variant with open password storage
         auth.userDetailsService(userDetailsService);
 
@@ -39,17 +45,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/users/**").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/random/get").access("permitAll()")
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin()
-                .and().csrf().disable();
+                .and()
+                // We filter the api/login requests
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+//        http.authorizeRequests()
+//                //.antMatchers("/users/**").access("hasRole('ROLE_ADMIN')")
+//                .antMatchers("/random/get").access("permitAll()")
+//                .anyRequest().authenticated()
+//                .and().formLogin()
+//                .and().csrf().disable();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        return encoder;
+//    }
 }
---
+++ src/main/java/com/yevhenii/configuration/filters/AccountCredentials.java
@@ -0,0 +1,25 @@
+package com.yevhenii.configuration.filters;
+
+/**
+ * Created by Yevhenii on 04.12.2017.
+ */
+public class AccountCredentials {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
